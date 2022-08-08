95ce8fde41cb4ca39c507b5b9fb4d9c6168bc9f4
Andrés Felipe Jaimes Sánchez <andres_ja_sa@outlook.com>
2022-08-08 15:04:29 -0500

    feat: add more functions

--- js/script.js
+++ js/script.js
@@ -13,6 +13,11 @@ const createArray = () => {
   console.log("output:", emptyArray);
   // output :
   // ['primer valor', '2 valor', '3 valor', empty × 2, 'cuarto valor']
+
+  let cars = ["Saab", "Volvo", "BMW"];
+  console.log(cars);
+  cars = ["Toyota", "Volvo", "Audi"]; // GOOD
+  console.log(cars);
 };
 
 const createFilledArray = () => {
@@ -27,9 +32,51 @@ const createFilledArray = () => {
   ];
   console.log("output: ", filledArray);
   // ['primer valor', '2 valor', '3 valor', empty × 2, 'cuarto valor']
+
+  const cars = ["Saab", "Volvo", "BMW"];
+  console.log(cars);
+  cars = ["Toyota", "Volvo", "Audi"]; // ERROR
+  console.log(cars);
+};
+
+const createArrayWithFunction = () => {
+  // la declaracion de new Array no permite crear campos vacios ya que crea un array nuevo a partir de unos valores dados y en orden
+  const newArray = new Array(
+    "primer valor",
+    "2 valor",
+    3 + " valor",
+    "cuarto valor"
+  );
+  console.log(newArray);
+
+  // ADVERTENCIA -> el metodo new Array puede generar errores inesperados
+  const newArray2 = new Array(20);
+  console.log("array con 20 espacios vacios", newArray2);
 };
 
 const arrayLength = () => {
   console.log("numeric array:", numericArray);
   console.log("numeric array length:", numericArray.length);
+};
+
+const arrayAccess = () => {
+  console.log("numeric array:", numericArray);
+  // accede al registro del index = 1 es decir el segundo registro
+  console.log("index = 0:", numericArray[0]);
+  // accede al registro del index = 1 es decir el segundo registro
+  console.log("index = 1:", numericArray[1]);
+  // accede al ultimo registro, como el primer indice es 0 por logica
+  // el ultimo registro es igual a la longitud del array - 1
+  console.log("index = last:", numericArray[numericArray.length - 1]);
+};
+
+const knowIfIsArray = () => {
+  console.log("numeric array:", numericArray);
+  // typeof reconoce los arrays como objetos
+  console.log("typeof numericArray", typeof numericArray);
+  // Array.isArray es un metodo de ECMAScript que reconoce si algo es un array
+  console.log("Array.isArray(numericArray)", Array.isArray(numericArray));
+  console.log('Array.isArray("soy un array?")', Array.isArray("soy un array?"));
+  // instandeof verifica si un objeto en su cadena de prototipos contiene la propiedad prototype de un constructor dado
+  console.log("numericArray instanceof Array", numericArray instanceof Array);
 };
